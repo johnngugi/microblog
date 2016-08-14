c93b2430e80264dfe2d94e896f971f647fe215e9
johnngugi <johnngugi777@gmail.com>
2016-08-14 22:03:37 +0300

    Add working files and folders

---
+++ config.py
@@ -0,0 +1,12 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/microblog'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ manage.py
@@ -0,0 +1,8 @@
+import app
+from app import db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
