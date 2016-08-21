f535b0b7e9b4c5f14a975ee44318e57763f1cad1
johnngugi <johnngugi777@gmail.com>
2016-08-21 17:53:35 +0300

    Add ui for main page, add new form

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from app import app, lm, db
-from .forms import LoginForm, EditForm, RegistrationForm
-from .models import User, Permission
+from .forms import RegistrationForm, LoginForm, EditProfileForm, EditProfileAdminForm, PostForm
+from .models import User, Role, Permission, Post
 from decorators import admin_required, permission_required
 from flask_login import login_user, logout_user, current_user, login_required
 
@@ -20,9 +20,16 @@ def before_request():
 @app.route('/index/')
 @login_required
 def index():
-    return render_template('index.html',
-                           title='Home',
-                           )
+    form = PostForm()
+    if current_user.can(Permission.WRITE_ARTICLES) and \
+            form.validate_on_submit():
+        post = Post(body=form.body.data,
+                    author=current_user._get_current_object())
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -67,19 +74,47 @@ def user(username):
 
 @app.route('/edit', methods=['GET', 'POST'])
 @login_required
-def edit():
-    form = EditForm(g.user.nickname)
+def edit_profile():
+    form = EditProfileForm()
     if form.validate_on_submit():
-        g.user.nickname = form.nickname.data
+        g.user.name = form.name.data
+        g.user.location = form.location.data
         g.user.about_me = form.about_me.data
-        db.session.add(g.user)
+        db.session.add(user)
         db.session.commit()
-        flash('Your changes have been save')
-        return redirect(url_for('edit'))
-    else:
-        form.nickname.data = g.user.nickname
-        form.about_me.data = g.user.about_me
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=g.user.username))
+    form.name.data = g.user.name
+    form.location.data = g.user.location
+    form.about_me.data = g.user.about_me
     return render_template('edit.html', form=form)
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(url_for('user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit.html', form=form, user=user)
 
 
 @app.route('/admin')
