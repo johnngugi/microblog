6013824edd6fe7475ba0d8c1e7d1e04f50e2a73a
johnngugi <johnngugi777@gmail.com>
2016-08-23 08:47:51 +0300

    Chnage database uri

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/microblog'
-SQLALCHEMY_DATABASE_URI = 'postgres://vfdmynrxdbkibv:q86zmPG0sAsWpTHiZMqGpL1rqn@ec2-54-225-100-236.compute-1.amazonaws.com:5432/dfs80bqrqhdp0g'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/microblog'
+# SQLALCHEMY_DATABASE_URI = 'postgres://vfdmynrxdbkibv:q86zmPG0sAsWpTHiZMqGpL1rqn@ec2-54-225-100-236.compute-1.amazonaws.com:5432/dfs80bqrqhdp0g'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 FLASKY_ADMIN = 'johnngugi'
--- manage.py
+++ manage.py
@@ -1,13 +1,13 @@
 from app import manager
-from flask_migrate import upgrade
-from app.models import Role, User
-
-
-@manager.command
-def deploy():
-    """Run deployment tasks."""
-    # migrate database to latest revision
-    upgrade()
-    # create user roles
-    Role.insert_roles()
-    manager.run()
+# from flask_migrate import upgrade
+# from app.models import Role, User
+#
+#
+# @manager.command
+# def deploy():
+#     """Run deployment tasks."""
+#     # migrate database to latest revision
+#     upgrade()
+#     # create user roles
+#     Role.insert_roles()
+manager.run()
