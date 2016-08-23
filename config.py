8a672107828c23789e6a52383b16753c65e9904b
johnngugi <johnngugi777@gmail.com>
2016-08-23 14:12:56 +0300

    Chnage db

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/microblog'
-# SQLALCHEMY_DATABASE_URI = 'postgres://vfdmynrxdbkibv:q86zmPG0sAsWpTHiZMqGpL1rqn@ec2-54-225-100-236.compute-1.amazonaws.com:5432/dfs80bqrqhdp0g'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///microblog.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 FLASKY_ADMIN = 'johnngugi'
