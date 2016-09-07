d854d130ef4e371d78fde10435dac4adf8317d0f
johnngugi <johnngugi777@gmail.com>
2016-09-07 16:23:19 +0300

    Add edit blog posts feature

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'secret-key'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
