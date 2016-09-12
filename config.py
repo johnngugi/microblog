382a75f247da7ae8f0f7f653178d5c66de6bd716
johnngugi <johnngugi777@gmail.com>
2016-09-12 17:06:53 +0300

    Change db

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(basedir, 'app.db') or 'sqlite:////app.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
